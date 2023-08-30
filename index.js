7a2fdae9c3c7307071d40f73367d1c5fdc506f3a
AramPetrosyan07 <aspetrosyan@mail.ru>
2023-08-30 13:34:34 +0400

    getLoads public

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post(
 );
 
 app.post("/load/add", checkAuth, LoadController.addNewLoad);
-app.get("/load/get", checkAuth, LoadController.getLoads);
+app.get("/load/get", LoadController.getLoads);
 app.post("/load/getUserLoads", checkAuth, LoadController.getUserLoads);
 app.post("/load/getDetail", checkAuth, LoadController.getDetailLoad);
 app.post("/load/updateLoad", checkAuth, LoadController.updateLoad);
