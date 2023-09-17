e03b84536b0606f7642c4c7b9817cf52a8a293e9
AramPetrosyan07 <aspetrosyan@mail.ru>
2023-09-17 12:26:32 +0400

    fix detail load

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post(
 app.post("/load/add", checkAuth, LoadController.addNewLoad);
 app.get("/load/get", LoadController.getLoads);
 app.post("/load/getUserLoads", checkAuth, LoadController.getUserLoads);
-app.post("/load/getDetail", checkAuth, LoadController.getDetailLoad);
+app.post("/load/getDetail", LoadController.getDetailLoad);
 app.post("/load/updateLoad", checkAuth, LoadController.updateLoad);
 app.post("/load/deleteLoad", checkAuth, LoadController.deleteLoad);
 
